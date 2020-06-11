cc17df9694f0f024423676d4a92c144a0720e003
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-12 00:22:09 +0500

    Добавляет пакет prop-types

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Main = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {title, genre, releaseYear} = props.promoCard;
 
   return (
