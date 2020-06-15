c31ae80d4a383f8430cfdf6130b7b429a468fbad
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-16 00:03:20 +0500

    Добавляет моковые данные для фильмов

---
+++ src/mocks/films.js
@@ -0,0 +1,42 @@
+export const FILMS_DATA = [
+  {
+    title: `Batman Begins`,
+    preview: `https://thumbs.dfs.ivi.ru/storage2/contents/d/8/d885cde980d7ff1041f8b016f516cd.jpg`,
+    genre: `Fantasy`
+  },
+  {
+    title: `Joker`,
+    preview: `https://thumbs.dfs.ivi.ru/storage15/contents/7/d/726a60465521a979da1e41c3a01815.jpg`,
+    genre: `Drama`
+  },
+  {
+    title: `The Master`,
+    preview: `https://thumbs.dfs.ivi.ru/storage3/contents/f/9/19868e92281c0796716e71f1290b3c.jpg`,
+    genre: `Drama`
+  },
+  {
+    title: `Inception`,
+    preview: `https://thumbs.dfs.ivi.ru/storage8/contents/a/0/24c373730abbad005d8d11f73efd45.jpg`,
+    genre: `Fantasy`
+  },
+  {
+    title: `Casino Royale`,
+    preview: `https://thumbs.dfs.ivi.ru/storage2/contents/5/6/32d48310fc021364e9d2585d19abd4.jpg`,
+    genre: `Thriller`
+  },
+  {
+    title: `The Gentlemen`,
+    preview: `https://thumbs.dfs.ivi.ru/storage32/contents/a/6/8199318552fd6212c08ee161449b86.jpg`,
+    genre: `Comedy`
+  },
+  {
+    title: `I, Robot`,
+    preview: `https://thumbs.dfs.ivi.ru/storage3/contents/7/2/ae76163737bc52df36316547b3ff23.jpg`,
+    genre: `Fantasy`
+  },
+  {
+    title: `Blade II`,
+    preview: `https://thumbs.dfs.ivi.ru/storage33/contents/c/5/27655671d79c82ab6225b70b2bdce0.jpg`,
+    genre: `Horror`
+  }
+];
