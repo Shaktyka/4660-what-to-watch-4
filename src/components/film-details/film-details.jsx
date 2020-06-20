844e5949050bf830db992ee4c3a9198196ad3c5d
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-20 21:47:47 +0500

    Расширяет моковые данные

--- src/components/film-details/film-details.jsx
+++ src/components/film-details/film-details.jsx
@@ -6,7 +6,7 @@ const FilmDetails = () => {
       <section className="movie-card movie-card--full">
         <div className="movie-card__hero">
           <div className="movie-card__bg">
-            <img className="./img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+            <img src="./img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,42 +2,146 @@ const FILMS_DATA = [
   {
     id: 1,
     title: `Bohemian Rhapsody`,
-    preview: `bohemian-rhapsody.jpg`
+    genre: `Drama`,
+    year: 2018,
+    preview: `bohemian-rhapsody.jpg`,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 8.6,
+    ratingCount: 240,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 2,
     title: `Dardjeeling Limited`,
-    preview: `dardjeeling-limited.jpg`
+    preview: `dardjeeling-limited.jpg`,
+    genre: `Drama`,
+    year: 2019,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 7.6,
+    ratingCount: 140,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 3,
     title: `Fantastic beasts: the crimes of Grindelwald`,
-    preview: `fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    preview: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    genre: `Fantasy`,
+    year: 2019,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 8.9,
+    ratingCount: 300,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 4,
     title: `Johnny English`,
-    preview: `johnny-english.jpg`
+    preview: `johnny-english.jpg`,
+    genre: `Biography`,
+    year: 2016,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 8.2,
+    ratingCount: 150,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 5,
     title: `Macbeth`,
-    preview: `macbeth.jpg`
+    preview: `macbeth.jpg`,
+    genre: `Crime`,
+    year: 2017,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 4.5,
+    ratingCount: 192,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 6,
     title: `Midnight Special`,
-    preview: `midnight-special.jpg`
+    preview: `midnight-special.jpg`,
+    genre: `Fighter`,
+    year: 2015,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 5.9,
+    ratingCount: 210,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 7,
     title: `Mindhunter`,
-    preview: `mindhunter.jpg`
+    preview: `mindhunter.jpg`,
+    genre: `Drama`,
+    year: 2018,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 6.7,
+    ratingCount: 410,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   },
   {
     id: 8,
     title: `Moonrise Kingdom`,
-    preview: `moonrise-kingdom.jpg`
+    preview: `moonrise-kingdom.jpg`,
+    genre: `Fantasy`,
+    year: 2014,
+    poster: `the-grand-budapest-hotel-poster.jpg`,
+    cover: `bg-the-grand-budapest-hotel.jpg`,
+    ratingScore: 7.5,
+    ratingCount: 80,
+    description: [
+      `Bohemian Rhapsody is a foot-stomping celebration of Queen, their music and singer Freddie Mercury.`,
+      `The film traces the meteoric rise of the band through their iconic songs and revolutionary sound.`,
+      `They reach unparalleled success, but in an unexpected turn Freddie, surrounded by darker influences.`
+    ],
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`]
   }
 ];
 
