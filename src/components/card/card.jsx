c6d18c8cd7948c59bd2bd4196145150d28d9fcb8
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-24 14:54:28 +0500

    Добавляет функциональный компонент VideoPlayer и импортирует его в компонент Card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import VideoPlayer from '../video-player/video-player.jsx';
 
 const Card = ({film, onFilmCardClick, onMouseEnterCard, onMouseLeaveCard}) => {
-  const {id, title, preview} = film;
+  const {id, title, preview, video} = film;
 
   const handleCardEnter = () => onMouseEnterCard(id);
 
@@ -20,6 +21,7 @@ const Card = ({film, onFilmCardClick, onMouseEnterCard, onMouseLeaveCard}) => {
         onClick={() => onFilmCardClick(id)}
       >
         <img src={`img/${preview}`} alt={title} width="280" height="175" />
+        <VideoPlayer video={video} poster={preview} />
       </div>
       <h3
         className="small-movie-card__title"
@@ -38,7 +40,8 @@ Card.propTypes = {
   film: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    preview: PropTypes.string.isRequired
+    preview: PropTypes.string.isRequired,
+    video: PropTypes.string.isRequired
   }).isRequired,
   onFilmCardClick: PropTypes.func.isRequired,
   onMouseEnterCard: PropTypes.func.isRequired,
--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,56 +1,21 @@
-import React, {PureComponent, createRef} from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from "prop-types";
 
-class VideoPlayer extends PureComponent {
-  constructor(props) {
-    super(props);
+const VideoPlayer = (props) => {
+  const {video, poster} = props;
 
-    this._videoRef = createRef();
-
-    this.state = {
-      progress: 0,
-      isPlaying: props.isPlaying,
-      isPaused: props.isPaused
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
-
-  componentWillUnmount() {
-    const video = this._videoRef.current;
-
-    video.src = ``;
-  }
-
-  render() {
-    const {video, poster} = this.props;
-
-    return (
-      <video
-        src={video}
-        poster={poster}
-        ref={this._videoRef}
-        muted
-      >
-        Sorry, your browser doesn&apos;t support embedded videos,
-        but don&apos;t worry, you can <a href={video}>download it</a>
-        and watch it with your favorite video player!
-      </video>
-    );
-  }
-
-  componentDidUpdate() {
-    // const video = this._videoRef.current;
-  }
-}
+  return (
+    <video
+      src={video}
+      poster={poster}
+      muted="muted"
+    />
+  );
+};
 
 VideoPlayer.propTypes = {
   video: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired,
-  isPlaying: PropTypes.bool.isRequired,
-  isPaused: PropTypes.bool.isRequired
+  poster: PropTypes.string.isRequired
 };
 
 export default VideoPlayer;
