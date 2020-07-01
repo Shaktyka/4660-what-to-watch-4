2dc51ee3de8ab5c21645ddd466588f27577fbcf8
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-07-01 12:51:23 +0500

    Передаёт отзывы в компонент отзывов

--- src/components/film-details/film-details.jsx
+++ src/components/film-details/film-details.jsx
@@ -10,51 +10,6 @@ const activeTab = `Overview`;
 const onTabClick = (/* tabName */) => {
   // console.log(tabName);
 };
-const reviews = [
-  {
-    text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European
-      kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
-    author: `Kate Muir`,
-    date: `2016-12-24`,
-    rating: `8,9`
-  },
-  {
-    text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves
-      in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added
-      a hint of gravitas to the mix, improving the recipe.`,
-    author: `Bill Goodykoontz`,
-    date: `2015-11-18`,
-    rating: `8,0`
-  },
-  {
-    text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40
-      minutes I wish I could take back.`,
-    author: `Amanda Greever`,
-    date: `2015-11-18`,
-    rating: `8,0`
-  },
-  {
-    text: `The mannered, madcap proceedings are often delightful, occasionally
-      silly, and here and there, gruesome and/or heartbreaking.`,
-    author: `Matthew Lickona`,
-    date: `2016-12-20`,
-    rating: `7,2`
-  },
-  {
-    text: `It is certainly a magical and childlike way of storytelling,
-      even if the content is a little more adult.`,
-    author: `Paula Fleri-Soler`,
-    date: `2016-12-20`,
-    rating: `7,6`
-  },
-  {
-    text: `It is certainly a magical and childlike way of storytelling,
-      even if the content is a little more adult.`,
-    author: `Marta Soler`,
-    date: `2016-12-22`,
-    rating: `7,0`
-  }
-];
 
 const tabs = [`Overview`, `Details`, `Reviews`];
 
@@ -71,7 +26,8 @@ const FilmDetails = ({filmData}) => {
     description,
     director,
     starring,
-    duration
+    duration,
+    reviews
   } = filmData;
 
   return (
@@ -250,7 +206,8 @@ FilmDetails.propTypes = PropTypes.shape({
   description: PropTypes.arrayOf(PropTypes.string).isRequired,
   director: PropTypes.string.isRequired,
   starring: PropTypes.arrayOf(PropTypes.string),
-  duration: PropTypes.number.isRequired
+  duration: PropTypes.number.isRequired,
+  reviews: PropTypes.array.isRequired
 }).isRequired;
 
 export default FilmDetails;
--- src/mocks/films.js
+++ src/mocks/films.js
@@ -4,52 +4,6 @@ export const promoFilmData = {
   releaseYear: 2014
 };
 
-const reviews = [
-  {
-    text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European
-      kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
-    author: `Kate Muir`,
-    date: `2016-12-24`,
-    rating: `8,9`
-  },
-  {
-    text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves
-      in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added
-      a hint of gravitas to the mix, improving the recipe.`,
-    author: `Bill Goodykoontz`,
-    date: `2015-11-18`,
-    rating: `8,0`
-  },
-  {
-    text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40
-      minutes I wish I could take back.`,
-    author: `Amanda Greever`,
-    date: `2015-11-18`,
-    rating: `8,0`
-  },
-  {
-    text: `The mannered, madcap proceedings are often delightful, occasionally
-      silly, and here and there, gruesome and/or heartbreaking.`,
-    author: `Matthew Lickona`,
-    date: `2016-12-20`,
-    rating: `7,2`
-  },
-  {
-    text: `It is certainly a magical and childlike way of storytelling,
-      even if the content is a little more adult.`,
-    author: `Paula Fleri-Soler`,
-    date: `2016-12-20`,
-    rating: `7,6`
-  },
-  {
-    text: `It is certainly a magical and childlike way of storytelling,
-      even if the content is a little more adult.`,
-    author: `Marta Soler`,
-    date: `2016-12-22`,
-    rating: `7,0`
-  }
-];
-
 export const FILMS_DATA = [
   {
     id: 1,
