03d29a8593f20d28fb9fb7ca5a3d3386c494a07e
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-07-19 11:51:57 +0500

    Убирает filterByGenre

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -44,8 +44,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   onGenreClick(genre) {
-    dispatch(ActionCreator.changeGenre(genre));
-    dispatch(ActionCreator.filterByGenre(genre));
+    dispatch(ActionCreator.setGenre(genre));
   }
 });
 
--- src/reducer/app-state/app-state.js
+++ src/reducer/app-state/app-state.js
@@ -15,29 +15,18 @@ const initialState = {
 };
 
 const ActionType = {
-  CHANGE_GENRE: `CHANGE_GENRE`,
-  FILTER_BY_GENRE: `FILTER_BY_GENRE`,
+  SET_GENRE: `SET_GENRE`,
   SET_SELECTED_FILM: `SET_SELECTED_FILM`,
   CHANGE_MOVIE_NAV_TAB: `CHANGE_MOVIE_NAV_TAB`,
   SORT_BY_GENRE: `SORT_BY_GENRE`
 };
 
-// Определяет объекты экшенов
 const ActionCreator = {
 
-  changeGenre: (genre = DEFAULT_GENRE) => {
+  setGenre: (genre = DEFAULT_GENRE) => {
     return (
       {
-        type: ActionType.CHANGE_GENRE,
-        payload: genre
-      }
-    );
-  },
-
-  filterByGenre: (genre = DEFAULT_GENRE) => {
-    return (
-      {
-        type: ActionType.FILTER_BY_GENRE,
+        type: ActionType.SET_GENRE,
         payload: genre
       }
     );
@@ -64,7 +53,7 @@ const ActionCreator = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ActionType.CHANGE_GENRE:
+    case ActionType.SET_GENRE:
       return extend(state, {
         genre: action.payload || DEFAULT_GENRE
       });
