7de1d58090809af6c50b11ba02694aed71e46bdd
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-07-21 20:44:12 +0500

    Добавляет ссылку на страницу логина

--- src/components/film-details/film-details.jsx
+++ src/components/film-details/film-details.jsx
@@ -104,7 +104,7 @@ const FilmDetails = (props) => {
                     <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
                   </div>
                   :
-                  <a href="sign-in.html" className="user-block__link">Sign in</a>
+                  <a href="/login" className="user-block__link">Sign in</a>
               }
             </div>
           </header>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -59,7 +59,7 @@ const Main = (props) => {
                   <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
                 </div>
                 :
-                <a href="sign-in.html" className="user-block__link">Sign in</a>
+                <a href="/login" className="user-block__link">Sign in</a>
             }
           </div>
         </header>
