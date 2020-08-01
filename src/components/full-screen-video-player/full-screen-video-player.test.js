b8bfd6833e291af75e85ace67a0672bea74c52a5
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-08-01 14:18:51 +0500

    Добавляет правки снепшот-теста для полноэкранного плеера

--- src/components/full-screen-video-player/full-screen-video-player.test.js
+++ src/components/full-screen-video-player/full-screen-video-player.test.js
@@ -3,15 +3,31 @@ import renderer from 'react-test-renderer';
 
 import FullScreenVideoPlayer from './full-screen-video-player.jsx';
 
+const MockComponent = () => {
+  return (
+    <div></div>
+  );
+};
+
 describe(`FullScreenVideoPlayer rendering`, () => {
 
   it(`FullScreenVideoPlayer renders correctly`, () => {
 
     const tree = renderer
       .create(
-          <FullScreenVideoPlayer />
-      )
-      .toJSON();
+          <FullScreenVideoPlayer
+            isPlay={true}
+            timeElapsed={0}
+            currentProgress={`0`}
+            onPlayPauseButtonClick={() => {}}
+            onFullScreenClick={() => {}}
+          >
+            <MockComponent/>
+          </FullScreenVideoPlayer>, {
+            createNodeMock: () => {
+              return {};
+            }
+          }).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
