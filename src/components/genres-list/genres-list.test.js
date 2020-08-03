55170fe12e7f3e18eef1c1dc75b3fda1b74dd880
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-08-03 11:26:58 +0500

    Правит снепшот-тесты App и GenresList

--- src/components/genres-list/genres-list.test.js
+++ src/components/genres-list/genres-list.test.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import NameSpace from "../../reducer/name-space";
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import GenresList from './genres-list.jsx';
 
-const genres = [`All genres`, `Crime`, `Sci-Fi`, `Drama`, `Comedy`];
+const genresList = [`All genres`, `Crime`, `Sci-Fi`, `Drama`, `Comedy`];
+const mockStore = configureStore([]);
+
+const store = mockStore({
+  [NameSpace.DATA]: {
+    films: [],
+    favoritesFilms: [],
+    promoFilm: {},
+    genres: [],
+  },
+  [NameSpace.APP_STATE]: {
+    isLoading: false,
+    selectedFilmId: 1,
+    genres: genresList,
+    genre: `All genres`
+  },
+  [NameSpace.USER]: {
+    authorizationStatus: `NO_AUTH`,
+    userData: {},
+  }
+});
 
 describe(`GenresList rendering`, () => {
 
   it(`GenresList renders correctly with the first active filter`, () => {
-    const activeGenre = `All genres`;
 
     const tree = renderer
       .create(
-          <GenresList
-            genres={genres}
-            activeGenre={activeGenre}
-            onGenreClick={() => {}}
-          />
-      )
-      .toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-
-  it(`GenresList renders correctly with the third active filter`, () => {
-    const activeGenre = `Sci-Fi`;
-
-    const tree = renderer
-      .create(
-          <GenresList
-            genres={genres}
-            activeGenre={activeGenre}
-            onGenreClick={() => {}}
-          />
-      )
-      .toJSON();
+          <Provider store={store}>
+            <GenresList
+              activeGenre={`All genres`}
+              onGenreClick={() => {}}
+            />
+          </Provider>, {
+            createNodeMock: () => {
+              return {};
+            }
+          }
+      ).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
 });
-
